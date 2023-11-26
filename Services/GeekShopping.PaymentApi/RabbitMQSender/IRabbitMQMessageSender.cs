459bc4c70b1c20a6c6eee44d76694d9cfc774bbe
matheuz-siqueira <matheusbatista.dev@gmail.com>
2023-11-26 18:42:27 -0300

    feat: Implementando utilização de ExchangeMessages

--- Services/GeekShopping.PaymentApi/RabbitMQSender/IRabbitMQMessageSender.cs
+++ Services/GeekShopping.PaymentApi/RabbitMQSender/IRabbitMQMessageSender.cs
@@ -4,5 +4,5 @@ namespace GeekShopping.PaymentApi.RabbitMQSender;
 
 public interface IRabbitMQMessageSender
 {
-    void SendMessage(BaseMessage baseMessage, string queueName);
+    void SendMessage(BaseMessage baseMessage);
 }
